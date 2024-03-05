1b713daa811807210bb69ad70a70683e8804ef9f
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-03-05 13:14:07 -0500

    feat: login/register forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
-import path from 'path';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { OtroComponent } from './componentes/otro/otro.component';
 import { ErrorComponent } from './componentes/error/error.component';
 import { AcercaDEComponent } from './componentes/acerca-de/acerca-de.component';
+import { LoginComponent } from './pages/Auth/login/login.component';
+import { RegisterComponent } from './pages/Auth/register/register.component';
 
 export const routes: Routes = [
     {path: "home", component:InicioComponent},
     {path:"other", component:OtroComponent},
     {path:"about", component:AcercaDEComponent},
+    {path:"login", component:LoginComponent},
+    {path:"signup", component:RegisterComponent},
     {path:"",redirectTo:"home", pathMatch:"full"},
     {path:"",component:ErrorComponent}
 ];
