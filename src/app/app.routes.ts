757e2e3599c17a80397e2f6034da26de9772a762
alexdavid <joelarga2020@gmail.com>
2024-03-07 14:56:31 -0500

    ciclos de vida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { OtroComponent } from './componentes/otro/otro.component';
 import { ErrorComponent } from './componentes/error/error.component';
 import { AcercaDEComponent } from './componentes/acerca-de/acerca-de.component';
+import { CiclosDeVidaComponent } from './componentes/ciclos-de-vida/ciclos-de-vida.component';
 
 export const routes: Routes = [
     {path: "home", component:InicioComponent},
     {path:"other", component:OtroComponent},
     {path:"about", component:AcercaDEComponent},
+    {path:"livecicle",component:CiclosDeVidaComponent},
     {path:"",redirectTo:"home", pathMatch:"full"},
     {path:"",component:ErrorComponent}
+    
 ];
