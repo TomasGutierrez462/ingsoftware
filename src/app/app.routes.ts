4def0d34fc21da1374046d118eb76bdf6f5e9cf4
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-03-07 22:00:37 -0500

    fix: merge fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,25 +2,18 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { OtroComponent } from './componentes/otro/otro.component';
 import { ErrorComponent } from './componentes/error/error.component';
-<<<<<<< HEAD
-import { AcercaDEComponent } from './componentes/acerca-de/acerca-de.component';
-import { CiclosDeVidaComponent } from './componentes/ciclos-de-vida/ciclos-de-vida.component';
-=======
 import { AcercaDEComponent } from './pages/acerca-de/acerca-de.component';
+import { CiclosDeVidaComponent } from './componentes/ciclos-de-vida/ciclos-de-vida.component';
 import { LoginComponent } from './pages/Auth/login/login.component';
 import { RegisterComponent } from './pages/Auth/register/register.component';
->>>>>>> 87cc41a396323b07c33956442c7002e34a476447
 
 export const routes: Routes = [
     {path: "home", component:InicioComponent},
     {path:"other", component:OtroComponent},
     {path:"about", component:AcercaDEComponent},
-<<<<<<< HEAD
     {path:"livecicle",component:CiclosDeVidaComponent},
-=======
     {path:"login", component:LoginComponent},
     {path:"signup", component:RegisterComponent},
->>>>>>> 87cc41a396323b07c33956442c7002e34a476447
     {path:"",redirectTo:"home", pathMatch:"full"},
     {path:"",component:ErrorComponent}
     
