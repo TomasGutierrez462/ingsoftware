960a5096df72bea7ee0ce90d31cd2cdeab5d135b
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-03-08 09:00:24 -0500

    feat: requirements & swebok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,22 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { OtroComponent } from './componentes/otro/otro.component';
 import { ErrorComponent } from './componentes/error/error.component';
 import { AcercaDEComponent } from './pages/acerca-de/acerca-de.component';
-import { CiclosDeVidaComponent } from './componentes/ciclos-de-vida/ciclos-de-vida.component';
 import { LoginComponent } from './pages/Auth/login/login.component';
 import { RegisterComponent } from './pages/Auth/register/register.component';
+import { CiclosDeVidaComponent } from './pages/ciclos-de-vida/ciclos-de-vida.component';
+import { SwebokComponent } from './pages/swebok/swebok.component';
+import { RequirementsComponent } from './pages/requirements/requirements.component';
 
 export const routes: Routes = [
     {path: "home", component:InicioComponent},
     {path:"other", component:OtroComponent},
     {path:"about", component:AcercaDEComponent},
-    {path:"livecicle",component:CiclosDeVidaComponent},
+    {path:"lifecycle",component:CiclosDeVidaComponent},
     {path:"login", component:LoginComponent},
     {path:"signup", component:RegisterComponent},
+    {path:"swebok", component:SwebokComponent},
+    {path:"requeriments", component:RequirementsComponent},
     {path:"",redirectTo:"home", pathMatch:"full"},
-    {path:"",component:ErrorComponent}
+    {path:"**",component:ErrorComponent}
     
 ];
---
+++ src/app/pages/swebok/swebok.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-swebok',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './swebok.component.html',
+  styleUrl: './swebok.component.css'
+})
+export class SwebokComponent {
+
+}
